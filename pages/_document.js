98eeb81c8de2f0d4c0f3da0349ac5bc0d3905176
KyleLandon <kylepotteiger@gmail.com>
2025-04-26 18:27:25 -0500

    asdf

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,17 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
-          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Comic+Neue:wght@400;700&display=swap" 
+          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Comic+Neue:wght@400;700&display=swap&display=swap&text=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,%20!?@#%28%29%2F-" 
           rel="stylesheet"
+          media="print"
+          onLoad="this.media='all'"
         />
+        <noscript>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Comic+Neue:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </noscript>
       </Head>
       <body>
         <Main />
