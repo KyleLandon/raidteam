36baa09549b51126e8c915ffbbe683f056293a4d
KyleLandon <kylepotteiger@gmail.com>
2025-04-26 22:24:43 -0500

    adsf

--- next.config.js
+++ next.config.js
@@ -3,19 +3,7 @@ const nextConfig = {
     reactStrictMode: true,
     images: {
         unoptimized: true,
-    },
-    experimental: {
-        serverActions: true,
-    },
-    webpack: (config, { isServer }) => {
-        // Fix module resolution for aliases
-        config.resolve.alias = {
-            ...config.resolve.alias,
-        };
-        // Ensure proper module resolution
-        config.resolve.modules = ['node_modules', '.'];
-        return config;
-    },
+    }
 };
 
 module.exports = nextConfig;
--- pages/_document.js
+++ pages/_document.js
@@ -7,17 +7,9 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
-          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Comic+Neue:wght@400;700&display=swap&display=swap&text=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,%20!?@#%28%29%2F-" 
+          href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Comic+Neue:wght@400;700&display=swap" 
           rel="stylesheet"
-          media="print"
-          onLoad="this.media='all'"
         />
-        <noscript>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Comic+Neue:wght@400;700&display=swap"
-            rel="stylesheet"
-          />
-        </noscript>
       </Head>
       <body>
         <Main />
