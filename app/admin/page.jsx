3ccbe4325ee178a50fc3989ccdfbba81dbda0d01
KyleLandon <kylepotteiger@gmail.com>
2025-04-29 02:59:42 -0500

    asdf

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,8 +1,4 @@
-'use client';
 export const dynamic = "force-dynamic";
-export const dynamicParams = true;
-export function generateStaticParams() { return []; }
-
 import AdminClient from './AdminClient';
 
 export default function AdminPage() {
