66a3a982aaccaa62b42468a5cb6d69012421d177
KyleLandon <kylepotteiger@gmail.com>
2025-05-26 15:20:34 -0500

    adf

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,14 @@ import NextAuthSessionProvider from './providers/SessionProvider';
 
 export const metadata = {
     title: 'Raid Team - WoW Guild',
-    description: 'Raid Team is a no-drama, early AOTC guild that dives into Mythic.'
+    description: 'Raid Team is a no-drama, early AOTC guild that dives into Mythic.',
+    icons: {
+        icon: [
+            { url: '/favicon.svg', type: 'image/svg+xml' },
+            { url: '/images/raidteam.png', sizes: '32x32', type: 'image/png' }
+        ],
+        apple: '/images/raidteam.png',
+    }
 };
 
 export default function RootLayout({ children }) {
@@ -12,6 +19,8 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <link rel="icon" href="/images/raidteam.png" sizes="32x32" type="image/png" />
+                <link rel="apple-touch-icon" href="/images/raidteam.png" />
                 <script src="/js/script.js" defer></script>
             </head>
             <body>
